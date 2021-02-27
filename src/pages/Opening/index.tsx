83b6ada5858b613e156152c475fbbc6eda75b133
CHI FENG | Cliff <ga021396@gmail.com>
2021-02-28 01:15:31 +0900

    update opening

--- src/pages/Opening/index.tsx
+++ src/pages/Opening/index.tsx
@@ -5,18 +5,27 @@ import { Hero } from "../../store/hero/type";
 import { useState } from "react";
 import { fetch } from "../../store/hero/action";
 
+const snow = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
 function Opening() {
   const dispatch = useDispatch();
 
   return (
-    <div className="scene-container">
-      <div className="btnCon">
-        <button className="Btn" onClick={() => {}}>
-          開始
-        </button>
+    <div className="container">
+      {snow.map(() => (
+        <div className="svg"></div>
+      ))}
 
-        <div className="Btn" onClick={() => {}}>
-          說明
+      <div className={"titleContainer"}>
+        <div className={"title"}>Re:Sam</div>
+        <div className="btnCon">
+          <button className="Btn" onClick={() => {}}>
+            開始
+          </button>
+
+          <div className="Btn" onClick={() => {}}>
+            說明
+          </div>
         </div>
       </div>
     </div>
